ec24429b81f243a9ed3f40a3c0f7c8474761a730
Mahmood <boordel@outlook.com>
2023-05-03 14:23:25 +0330

    Some changes in DBContext registeration on Program file

--- Post/Post.Api/Program.cs
+++ Post/Post.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Post.Infra;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,14 @@ builder.Services.AddSwaggerGen();
 // Add Database Context
 builder.Services.AddDbContext<PostDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PostDB"),
-        b => b.MigrationsAssembly("Post.Api"))
+       sqlOptions =>
+       {
+           sqlOptions.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+           sqlOptions.EnableRetryOnFailure(
+               maxRetryCount: 10,
+               maxRetryDelay: TimeSpan.FromSeconds(30),
+               errorNumbersToAdd: null);
+       })
 );
 
 var app = builder.Build();
